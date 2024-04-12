c5775029cfcbefcfa213eefef2d3fefb47b744ca
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-12 20:42:15 -0300

    feat: Create view editar_registro

--- projetc_animal_registry/register/views.py
+++ projetc_animal_registry/register/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Animal
+from .forms import AnimalForm
 
-def index(request): 
-    return HttpResponse('Animal register test!')
+def editar_registro(request, cliente_id):
+    cliente = Animal.objects.get(id=cliente_id)
+    if request.method == 'POST':
+        form = AnimalForm(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('registro_editado_sucesso')
+    else:
+        form = AnimalForm(instance=cliente)
+    return render(request, 'editar_registro.html', {'form': form})
+
 
