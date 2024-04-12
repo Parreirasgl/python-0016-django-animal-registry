4eb8fc97b92beeba12e0b43e9304c3983eef3430
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-12 20:18:36 -0300

    feat: Create model Animal

--- projetc_animal_registry/register/models.py
+++ projetc_animal_registry/register/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    nome = models.CharField(max_length=100)
+    código = models.CharField(max_length=200)
+    historico = models.TextField()
+
