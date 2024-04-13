2a9e2101164b83f0a6ff3f661a766806f58bda0e
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-13 05:02:58 -0300

    refactor: small refactorings

--- projetc_animal_registry/register/urls.py
+++ projetc_animal_registry/register/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("busca", views.buscar, name='path_buscar'),
     path("<nome_animal>/edicao", views.editar_registro, name='path_editar_registro'),
     path("confirma", views.confirmar, name='path_confirmar'),
-    # path("<nome>/confirma", views.confirmar1, name='path_confirmar1'),
     ]
