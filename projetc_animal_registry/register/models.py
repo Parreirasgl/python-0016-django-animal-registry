26e5e3320ce6bb755db46da24129c6123c496227
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-12 21:33:49 -0300

    feat: Override function of the model Animal that indicates instance name

--- projetc_animal_registry/register/models.py
+++ projetc_animal_registry/register/models.py
@@ -4,4 +4,6 @@ class Animal(models.Model):
     nome = models.CharField(max_length=100)
     codigo = models.CharField(max_length=200)
     historico = models.TextField()
+    def __str__(self): 
+        return f'-- {self.nome}'
 
