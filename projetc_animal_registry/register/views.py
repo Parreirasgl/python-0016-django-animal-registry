e04f7760e0a04109d3defa20e247c04332b9fabc
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-12 21:19:53 -0300

    refactor: Change variable names in view

--- projetc_animal_registry/register/views.py
+++ projetc_animal_registry/register/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from .models import Animal
 from .forms import AnimalForm
 
-def editar_registro(request, cliente_id):
-    cliente = Animal.objects.get(id=cliente_id)
+def editar_registro(request):
+    animal = Animal.objects.get(codigo=1)
     if request.method == 'POST':
-        form = AnimalForm(request.POST, instance=cliente)
+        form = AnimalForm(request.POST, instance=animal)
         if form.is_valid():
             form.save()
             return redirect('confirmacao')
     else:
-        form = AnimalForm(instance=cliente)
+        form = AnimalForm(instance=animal)
     return render(request, 'editar_registro.html', {'form': form})
 
 def confirmacao(request):
