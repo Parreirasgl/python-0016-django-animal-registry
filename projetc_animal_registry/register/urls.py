d326c823775ad792af7bf472ff3610a59d150d3c
Guilherme Parreiras <parreirasgl@hotmail.com>
2024-04-12 21:16:02 -0300

    feat: Create editar_registro.html e sucesso.html

--- projetc_animal_registry/register/urls.py
+++ projetc_animal_registry/register/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')]
+    path("", views.editar_registro, name='editar_registro'),
+    path('/success', views.confirmacao, name='registro_editado_sucesso'),
+    ]
--- projetc_animal_registry/register/views.py
+++ projetc_animal_registry/register/views.py
@@ -8,9 +8,11 @@ def editar_registro(request, cliente_id):
         form = AnimalForm(request.POST, instance=cliente)
         if form.is_valid():
             form.save()
-            return redirect('registro_editado_sucesso')
+            return redirect('confirmacao')
     else:
         form = AnimalForm(instance=cliente)
     return render(request, 'editar_registro.html', {'form': form})
 
-
+def confirmacao(request):
+    return render(request, '/successo.html', {
+        })
